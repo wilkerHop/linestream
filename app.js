c458241b1c5a3d373b2db6fbc57cb2f41394953d
Wilker Ribeiro <rekliw97@gmail.com>
2024-12-30 17:24:36 -0300

    fix: point creating logic

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
 const Line = {
   get lastPointPosition() {
-    return this.points.at(-1);
+    return this.points.at(-1).currentPosition;
   },
 
   randomSign(probabilityNegative = 0.5) {
     if (probabilityNegative < 0 || probabilityNegative > 1) {
-      throw new Error('A probabilidade deve estar entre 0 e 1.');
+      throw new Error('Probability most be a number between 0 and 1.');
     }
 
     return Math.random() < probabilityNegative ? -1 : 1;
-  },
-
-  generateNewPosition(previewPosition) {
-    if (!previewPosition) {
-      throw new Error('No preview position provided');
-    }
-
-    return previewPosition - this.generateNewPosition();
   },
 
   createNextPoint(times = 1) {
     for (let index = 0; index < times; index++) {
       this.points.push({
         previewPosition: this.lastPointPosition,
-        currentPosition: this.generateNewPosition(this.lastPointPosition),
+        currentPosition: this.lastPointPosition - this.randomSign(),
       });
     }
   },
